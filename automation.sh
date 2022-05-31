d06a0468a4bf88b0a0f575bfe9c9e0552090f987
root <root@ip-172-31-29-69.ec2.internal>
2022-05-31 20:18:48 +0000

    new changes

--- automation.sh
+++ automation.sh
@@ -1,4 +1,5 @@
 
+
 sudo apt update -y
 
 if [ $( dpkg --status apache2 | grep -c "ok installed" ) -eq 0 ];
@@ -27,9 +28,36 @@ fi
 
 
 tar -cf shubham-httpd-logs-$(date "+%d%m%Y-%H%M%S").tar -C /var/log/apache2 access.log error.log
-mv shubham-httpd-logs* /tmp
+cp shubham-httpd-logs* /tmp
 
 aws s3 cp /tmp/shubham-httpd-logs-$(date "+%d%m%Y-%H%M%S").tar s3://upgrad-shubham
+
+#test –f /var/www/html/inventory.html
+if [  -f "/var/www/html/inventory.html" ];
+then
+	echo "file found"
+else
+	echo "file not found"
+	touch /var/www/html/inventory.html
+	echo "Log Type	 Time Created 	Type 	Size" >> /var/www/html/inventory.html
+
+fi
+ls -lh shubham-httpd-logs* | awk '{print $5}' 
+lgtyp='httpd-logs'
+typ='tar'
+echo $lgtyp$'\t'$(date "+%d%m%Y-%H%M%S")$'\t'$typ$'\t'$(ls -lh shubham-httpd-logs* | awk '{print $5}') >> /var/www/html/inventory.html
+
+
+rm shubham-httpd-logs*
+if [ -f "/etc/cron.d/automation" ];
+then
+	continue
+else
+	touch /etc/cron.d/automation
+	printf "0 0 * * * root /root/Automation_Project/auotmation.sh" > /etc/cron.d/automation
+fi
+
+
 
 
 
